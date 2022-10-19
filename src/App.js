bbf7048df58bffbb48ad23e471d20e6cd5e2219d
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2022-10-19 18:44:51 -0400

    added tick, compoundwillmount and compounddidmount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock'
 
 function App() {
+    
+    //example 1
+    const name = "Ryan Chevarria";
+    const element = <h1>Hello, {name}</h1>;
+    
+    //example 2
+    function formatName(user) {
+      return user.firstName + ' ' + user.lastName;
+    }
+
+    const user = {
+      firstName: 'Harper',
+      lastName: 'Perez'
+    };
+
+    const element1 = (
+      <h1>
+        Hello, {formatName(user)}!
+      </h1>
+    );
+    
+    //example 3
+    function getGreeting(user) {
+      if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+      }
+      return <h1>Hello, Stranger.</h1>;
+    }
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          {element1}
     </div>
   );
 }
